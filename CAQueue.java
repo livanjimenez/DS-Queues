f608881b6871e238ab529284265c801e941b6287
unknown <jimenez.livan14@gmail.com>
2019-05-23 01:36:33 -0400

    caqueue

--- CAQueue.java
+++ CAQueue.java
@@ -3,28 +3,45 @@ import java.util.ArrayList;
 public class CAQueue {
     private int front;
     private int back;
+    ArrayList<Integer> circularArray;
+    private int capacity;
 
-    public int getFront(int front) {
-        return this.front = front;
+    public CAQueue(int capacity) {
+        this.capacity = capacity;
+        back = capacity - 1;
+        front = 0;
+        circularArray = new ArrayList<>(capacity);
+
+        // Check what's inside the array
+        System.out.println(circularArray);
     }
 
-    public int getBack(int back) {
-        return this.back = back;
+    boolean isFull(CAQueue caQueue) {
+        return (circularArray.size() == caQueue.capacity);
     }
 
-    ArrayList<Integer> queue = new ArrayList<Integer>();
-
-    public void startQueue() {
-        for (int i = 0; i < 10; i++) 
-            queue.add(i);
-        
-        System.out.println(queue);
+    public int getFront() {
+        return this.front;
     }
 
-    public void enqueue() {
+    public int getBack() {
+        return this.back;
+    }
+
+    // Adds object to the queue
+    public void enqueue(int item) {
+        if (isFull(this))
+            return;
+        this.back = (this.back + 1) % this.capacity;
+
+
+        // https://www.geeksforgeeks.org/queue-set-1introduction-and-array-implementation/
+        // https://docs.oracle.com/middleware/1213/coherence/java-reference/com/tangosol/util/CircularArrayList.html
+
 
     }
 
+    // Removes object from the queue
     public void dequeue() {
 
     }
--- Main.java
+++ Main.java
@@ -1,13 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         new Main();
-
     }
 
     public Main() {
-       CAQueue c = new CAQueue();
 
-       c.startQueue();
-       c.enqueue();
     }
 }
